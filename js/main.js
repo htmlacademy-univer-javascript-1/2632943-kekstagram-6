b3ea29d2a0ec66e592eed85b4ff94b2d62a3f199
Vladuu3 <vladisupov79@mail.ru>
2025-10-20 19:14:52 +0500

    4.10 ver 2

--- js/main.js
+++ js/main.js
@@ -1,68 +1,86 @@
-const names = ['Артём', 'Мария', 'Иван', 'Анна', 'Дмитрий', 'Елена', 'Сергей', 'Ольга'];
+const NAMES = [
+  'Артём', 'Мария', 'Иван', 'Анна', 'Дмитрий',
+  'Елена', 'Сергей', 'Ольга', 'Алексей', 'Наталья',
+  'Павел', 'Юлия', 'Михаил', 'Екатерина', 'Андрей'
+];
 
-const messages = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат.',
-  'Лица у людей на фотке перекошены!'
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const descriptions = [
-  'Красивый закат', 'Горный пейзаж', 'Улочки города',
-  'Кофе и книга', 'Прогулка в парке', 'Ночной город',
-  'Цветущий сад', 'Путешествие', 'Встреча с друзьями',
-  'Мой питомец', 'Уютный вечер', 'Летние краски'
+const DESCRIPTIONS = [
+  'Прекрасный закат на море',
+  'Горный пейзаж в утреннем тумане',
+  'Улочки старого города',
+  'Кофе и книга в уютном кафе',
+  'Прогулка по осеннему парку',
+  'Архитектура современного мегаполиса',
+  'Цветущий сад весной',
+  'Ночной город с высоты птичьего полёта',
+  'Путешествие по неизведанным тропам',
+  'Моменты счастливой жизни',
+  'Удивительные формы природы',
+  'Искусство в повседневности',
+  'Вкусные блюда домашней кухни',
+  'Спортивные достижения и тренировки',
+  'Творческие проекты и хобби',
+  'Встреча с друзьями',
+  'Пушистые питомцы и их проделки',
+  'Уютные вечера дома',
+  'Приключения в новых местах',
+  'Красота в мелочах',
+  'Вдохновляющие пейзажи',
+  'Моменты тишины и спокойствия',
+  'Яркие краски лета',
+  'Зимняя сказка за окном',
+  'Магия золотой осени'
 ];
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
-function getRandomElement(array) {
-  return array[getRandomNumber(0, array.length - 1)];
-}
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-let commentIdCounter = 1;
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
 
-function createComment() {
-  return {
-    id: commentIdCounter++,
-    avatar: `img/avatar-${  getRandomNumber(1, 6)  }.svg`,
-    message: getRandomElement(messages),
-    name: getRandomElement(names)
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
   };
-}
+};
 
-function createPhoto(id) {
-  const commentsCount = getRandomNumber(0, 30);
-  const comments = [];
+const generateCommentId = createIdGenerator();
 
-  for (let i = 0; i < commentsCount; i++) {
-    comments.push(createComment());
-  }
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES)
+});
+
+const createPhoto = (index) => {
+  const photoId = index + 1;
+  const commentsCount = getRandomInteger(0, 30);
 
   return {
-    id: id,
-    url: `photos/${  id  }.jpg`,
-    description: getRandomElement(descriptions),
-    likes: getRandomNumber(15, 200),
-    comments: comments
+    id: photoId,
+    url: `photos/${photoId}.jpg`,
+    description: DESCRIPTIONS[index],
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({length: commentsCount}, createComment)
   };
-}
+};
 
-function generatePhotos() {
-  const photos = [];
+const generatePhotos = () => Array.from({length: 25}, (_, index) => createPhoto(index));
 
-  for (let i = 1; i <= 25; i++) {
-    photos.push(createPhoto(i));
-  }
-
-  return photos;
-}
-
-const photoArray = generatePhotos();
-
-// eslint-disable-next-line no-console
-console.log(photoArray);
+export { generatePhotos, getRandomInteger, getRandomArrayElement };
