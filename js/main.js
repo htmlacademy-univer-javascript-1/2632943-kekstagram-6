09210a9a539b02153f68218994e0a3d40f02b5e6
Vladuu3 <vladisupov79@mail.ru>
2025-12-15 00:48:09 +0500

    правки

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { renderThumbnails } from './thumbnail.js';
 import { getData } from './api.js';
+import { initFilter } from './filter.js';
 import { showAlert } from './util.js';
 import './form.js';
 
 getData()
   .then((pictures) => {
     renderThumbnails(pictures);
+    initFilter(pictures, renderThumbnails);
   })
   .catch((err) => {
     showAlert(err.message);
